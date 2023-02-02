b8b8641ab2e6c9d5df111e1497291217a2c65611
KirtanManek <manekkirtan@gmail.com>
2023-02-02 19:25:28 +0530

    added login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -29,9 +29,6 @@ class _LoginScreen extends State<LoginScreen> {
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color(0xff1f005c),
-            Color(0xff5b0060),
-            Color(0xff870160),
             Color(0xffac255e),
             Color(0xffca485c),
             Color(0xffe16b5c),
@@ -42,114 +39,120 @@ class _LoginScreen extends State<LoginScreen> {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextFormField(
-                  controller: userNameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'User Name',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'User Name cannot be empty';
-                    } else if (value.length < 3) {
-                      return 'UserName is too short';
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: TextFormField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    counterStyle: TextStyle(backgroundColor: Colors.teal),
-                    labelText: 'Password',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Password cannot be empty';
-                    } else if (value.length < 5) {
-                      return 'Password is too short';
-                    } else {
-                      bool result = validatePassword(value);
-                      if (result) {
-                        return null;
-                      } else {
-                        return "Password should contain at least \none Capital, \none small letter, \none Number & \none Special Character";
-                      }
-                    }
-                  },
-                ),
-              ),
-              Container(
-                  height: 50,
-                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.tealAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        print(userNameController.text);
-                        print(passwordController.text);
-                      }
-                    },
-                  )),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Form(
+            key: formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: ListView(
                 children: <Widget>[
-                  const Text(
-                    'Does not have account?',
-                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(10),
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
-                    child: TextButton(
-                      child: const Text(
-                        'Sign up',
-                        style:
-                            TextStyle(fontSize: 20, color: Colors.tealAccent),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: TextFormField(
+                      controller: userNameController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'User Name',
                       ),
-                      onPressed: () {
-                        //signup screen
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'User Name cannot be empty';
+                        } else if (value.length < 3) {
+                          return 'UserName is too short';
+                        } else {
+                          return null;
+                        }
                       },
                     ),
-                  )
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: TextFormField(
+                      obscureText: true,
+                      controller: passwordController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        counterStyle: TextStyle(backgroundColor: Colors.teal),
+                        labelText: 'Password',
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Password cannot be empty';
+                        } else if (value.length < 5) {
+                          return 'Password is too short';
+                        } else {
+                          bool result = validatePassword(value);
+                          if (result) {
+                            return null;
+                          } else {
+                            return "Password should contain at least \none Capital, \none small letter, \none Number & \none Special Character";
+                          }
+                        }
+                      },
+                    ),
+                  ),
+                  Container(
+                      height: 50,
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.tealAccent,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            print(userNameController.text);
+                            print(passwordController.text);
+
+                          }
+                        },
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        'Does not have account?',
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(4, 0, 0, 0),
+                        child: TextButton(
+                          child: const Text(
+                            'Sign up',
+                            style:
+                                TextStyle(fontSize: 20, color: Colors.tealAccent),
+                          ),
+                          onPressed: () {
+                            //signup screen
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
